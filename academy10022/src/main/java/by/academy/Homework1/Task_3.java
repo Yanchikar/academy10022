bc8a717f0a48726c88fcf4b2c18d76b21a870ac0
Yana <ignatovichyana9@gmail.com>
2022-03-18 18:38:07 +0300

    -

---
+++ academy10022/src/main/java/by/academy/Homework1/Task_3.java
@@ -0,0 +1,69 @@
+package by.academy.Homework1;
+
+import java.util.Scanner;
+
+    public class Task_3 {
+
+        public static void main(String[] args) {
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("Введите число от 1 до 10:");
+            int num = s.nextInt();
+
+            if (num <= 0 | num >= 11) {
+                System.out.println("Число " + num + " не в диапазоне. Попробуйте еще.");
+            } else if (num == 1) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 2) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 3) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 4) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 5) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 6) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 7) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 8) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 9) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            } else if (num == 10) {
+                System.out.println("Вы ввели число " + num);
+                for (int i = 1; i < 11; i++) {
+                    System.out.println(num * i);
+                }
+            }
+            s.close();
+        }
+    }
+
---
+++ academy10022/src/main/java/by/academy/Homework1/Task_4.java
@@ -0,0 +1,16 @@
+package by.academy.Homework1;
+
+public class Task_4 {
+
+        public static void main(String[] args) {
+
+            int i = 2;
+            while (i < 1000000) {
+                i = i * 2;
+                System.out.println(i);
+
+            }
+
+        }
+    }
+
